188ef193d1167355f7d5b2f2c7e1d319acde371e
Mareme Samate <mareme.samate@daviestech.org>
2020-10-08 13:35:08 +0000

    done

---
+++ question1.py
@@ -0,0 +1,6 @@
+x= 1 
+y= int(input("Enter an integer "))
+
+print(x + y)
+print(x + y + x)
+print(x + y + x + x)
---
+++ question2.py
@@ -0,0 +1,5 @@
+f= float(input("Enter decimal number "))
+
+c= (f + 32/5 + 9/2)
+
+print("Sum is: " + str(c))
